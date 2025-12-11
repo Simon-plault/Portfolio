bcaaba1b3e630c063ee937b000336d665b1abe6d
simon plault <splault@etudiant.univ-lr.fr>
2025-12-11 23:46:57 +0100

    [Feature] Ajout de Plusieur info sur moi

---
+++ src/components/About/About.jsx
@@ -0,0 +1,24 @@
+import './About.css';
+
+function About() {
+    return (
+        <div>
+            <h1>Simon PLAULT</h1>
+            <ul>
+                  <li>Titre: Développeur Fullstack Junior.</li>
+            <li>Statut : Étudiant en 2e année de BUT Informatique</li>
+            <li>Âge : 20 ans</li>
+            <li>Localisation : La Rochelle, France</li>
+            <li>Description : "Je sais concevoir des applications web de A à Z. 
+                Je maîtrise le front-end (HTML, CSS, JS) et le back-end (PHP/Symfony, SQL). 
+                Curieux et sérieux, j'aime coder des projets concrets qui fonctionnent bien.".</li>
+            </ul>
+            
+
+              
+            
+        </div>
+    );
+}
+
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import './Home.css';
-
+import About from '../../components/About/About';
 function Home() {
     return (
+        <div>
         <p>COUCOU CES LA PAGE HOME</p>
+        <About />
+        </div>
     );
 }
+
+export default Home;
